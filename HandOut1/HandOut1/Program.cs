9ce3c456b7badfccba8a19b1ba9418c9569ca9df
Phan Hau <phuchau1206@gmail.com>
2021-09-11 21:09:17 +0700

    Handout1

---
+++ HandOut1/HandOut1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HandOut1
+{
+    class Name
+    {
+        static void Main(string[] args)
+        {
+            String  name;
+            Console.Write("Please enter your name: ");
+            name = Console.ReadLine();
+            Console.WriteLine("Hello {0}", name);
+        }
+    }
+}
---
+++ Integer_operation/Integer_operation/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Integer_operation
+{
+    class IntegerOperation
+    {
+        static void Main(string[] args)
+        {
+            int x, y, sum, sub, mul;
+            float div; 
+        
+            Console.WriteLine("Input x: ");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("x = " + x);
+            Console.WriteLine("Input y: ");
+            y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("y = " + y);
+
+            sum = x + y;
+            sub = x - y;
+            mul = x * y;
+            div = x / y;
+
+            Console.WriteLine("Sum\t = " + sum);
+            Console.WriteLine("Subtract = " + sub);
+            Console.WriteLine("Multiply = " + mul);
+            Console.WriteLine("Divide\t = " + div);
+            Console.ReadLine();
+        }
+    }
+}
