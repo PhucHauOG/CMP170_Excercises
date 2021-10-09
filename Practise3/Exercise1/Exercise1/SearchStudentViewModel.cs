43390289ab54527046066e05a2e037cc998ee9c2
Phan Hau <phuchau1206@gmail.com>
2021-10-09 16:57:30 +0700

    Student Management Application

---
+++ Practise3/Exercise1/Exercise1/SearchStudentViewModel.cs
@@ -0,0 +1,68 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Exercise1
+{
+    public class Student 
+    {
+        public int StudentId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Gender { get; set; }
+        public string Class { get; set; }
+        public Decimal Gpa { get; set; }
+    }
+
+    class SearchStudentViewModel : BaseViewModel
+    {
+        private string m_searchKeyword;
+        public string SearchKeyword {
+            get => m_searchKeyword;
+            set
+            {
+                m_searchKeyword = value;
+                OnPropertyChanged(nameof(SearchKeyword));
+            }
+        }
+
+        private string m_selectedClass;
+        public string SelectedClass
+        {
+            get => m_selectedClass;
+            set
+            {
+                m_selectedClass = value;
+                OnPropertyChanged(nameof(SelectedClass));
+            }
+        }
+
+        private Student m_selectedStudent;
+        public Student SelectedStudent
+        {
+            get => m_selectedStudent;
+            set
+            {
+                m_selectedStudent = value;
+                OnPropertyChanged(nameof(SelectedStudent));
+            }
+        }
+        public ObservableCollection<Student> Students {get; set; }
+        public ICommand SearchCommand { get; set; }
+        public ICommand ResetCommand { get; set; }
+        public ICommand OpenDetailCommand { get; set; }
+        public SearchStudentViewModel() 
+        {
+            var data = File.ReadAllText("Student_Data.json");
+            var students = JsonConvert.DeserializeObject<List<Student>>(data);
+            Students = new ObservableCollection<Student>(students);
+        }
+    }
+}
